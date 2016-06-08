0bc008f4bf7a84860cbf1beaa5754aabb2db5939
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-07 18:18:25 -0600

    add beginning of amazon oauth plus welcome controller

--- app/services/walmart_service.rb
+++ app/services/walmart_service.rb
@@ -3,7 +3,6 @@ class WalmartService
   def initialize
     @_conn = Faraday.new(url: "http://api.walmartlabs.com")
     @_conn.params["apiKey"] = ENV['WALMART_API']
-    @_conn.params["per_page"] = 1000
   end
 
   def get_categories
@@ -12,17 +11,11 @@ class WalmartService
 
   def get_items(category)
     parse(connection.get("/v1/paginated/items", category))
-    # x = parse(connection.get("/v1/paginated/items", {category: 91083}))
   end
 
   def get_next_item_page(path)
     parse(connection.get("#{path}"))
   end
-
-#   def stations_by_zip(params)
-#  response = connection.get("nearest", params)
-#  parse(response)[:fuel_stations]
-# end
 
   private
 
