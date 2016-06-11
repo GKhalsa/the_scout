65cff2549a9418e442ed25e4f3ee21a59e48740f
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-06-11 12:13:49 -0600

    add full category to amazon items then pair with walmart item

--- app/services/amazon_service.rb
+++ app/services/amazon_service.rb
@@ -5,14 +5,19 @@ class AmazonService
   end
 
   def get_items(item_upcs)
-    url = "http://webservices.amazon.com/onca/xml?AWSAccessKeyId=#{ENV['ACCESS_KEY_ID']}&AssociateTag=oascout-20&ItemId=#{item_upcs}&IdType=UPC&SearchIndex=All&Operation=ItemLookup&ResponseGroup=Large&Service=AWSECommerceService&Timestamp="
-    # url = "http://webservices.amazon.com/onca/xml?AWSAccessKeyId=#{ENV['ACCESS_KEY_ID']}&AssociateTag=oascout-20&ItemId=035011061689,635753490879&IdType=UPC&SearchIndex=All&Operation=ItemLookup&ResponseGroup=Large&Service=AWSECommerceService&Timestamp="
+    begin
+      url = "http://webservices.amazon.com/onca/xml?AWSAccessKeyId=#{ENV['ACCESS_KEY_ID']}&AssociateTag=oascout-20&ItemId=#{item_upcs}&IdType=UPC&SearchIndex=All&Operation=ItemLookup&ResponseGroup=Large&Service=AWSECommerceService&Timestamp="
+      # url = "http://webservices.amazon.com/onca/xml?AWSAccessKeyId=#{ENV['ACCESS_KEY_ID']}&AssociateTag=oascout-20&ItemId=035011061689,635753490879&IdType=UPC&SearchIndex=All&Operation=ItemLookup&ResponseGroup=Large&Service=AWSECommerceService&Timestamp="
 
-    url << Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+      url << Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    signed_url = create_signature(url)
-    response = connection.get(signed_url)
-    Crack::XML.parse(response.body)["ItemLookupResponse"]["Items"]["Item"] unless response.nil?
+      signed_url = create_signature(url)
+      response = connection.get(signed_url)
+      Crack::XML.parse(response.body)["ItemLookupResponse"]["Items"]["Item"] unless response.nil?
+    rescue
+      puts "sleepytime"
+      sleep(2)
+    end
   end
 
   # def get_item(item_upcs)
